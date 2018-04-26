196c602a76d225cf68127ac3a4e8055aae1e9fb2
Chris Harding <christopher.harding@gmx.com>
2018-04-26 10:48:16 -0400

    initial commit, basic server set-up

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to TubeFlix!');
+});
+
+app.use('*', (req, res) => {
+    res.send("Route not found");
+});
