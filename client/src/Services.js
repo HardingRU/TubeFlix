3962387c92eb0ed0ea2b26f1064e4543d8ef5b2e
Chris Harding <christopher.harding@gmx.com>
2018-05-16 09:49:08 -0400

    added thumbnails for search results to search page

--- client/src/Services.js
+++ client/src/Services.js
@@ -2,13 +2,6 @@ import axios from 'axios'
 
 class Services {
 
-  // search(query) {
-  //   return axios({
-  //     method: 'get',
-  //     url: `/api/search/${query}`,
-  //   })
-  // }
-
   getHomeData() {
     return axios({
       method: 'get',
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -11,29 +11,68 @@ class Search extends Component {
       apiDataLoaded: false,
       apiData: null
     }
+    this.renderResults = this.renderResults.bind(this);
+
   }
 
   componentDidMount() {
     services.search(this.props.match.params.query)
-      .then(data => {
-        console.log("search data", data)
+    .then(data => {
+      console.log("search data", data)
+      this.setState({
+        apiDataLoaded: true,
+        apiData: data
       })
-      .catch(err => {
-        console.log(err)
-      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 
+  renderResults() {
+    return this.state.apiData.data.data.map((el,i) => {
+      let link = "https://www.youtube.com/watch?v=" + el.id.videoId
+      return (
+          <div key={i}>
+            <a href={link}><img alt="" src={el.snippet.thumbnails.high.url} width="320" height="200"></img></a>
+            <a href={link}><p>{el.snippet.title}</p></a>
+          </div>
+      )
+    })
+  }
 
   render() {
-
-    console.log("props", this.props.match.params.query)
-
     return (
-      <h1>Search Results for {this.props.match.params.query}</h1>
-
+      <div>
+        {this.state.apiDataLoaded ? this.renderResults() : <h1>Loading...</h1>}
+      </div>
     )
+
+
+    // if(this.state.apiDataLoaded === true) {
+    //   return (
+    //   <div>
+    //     <h1>Search Results for {this.props.match.params.query}</h1>
+    //     <div className="row">
+    //       <div className="col-xs-6 col-md-3">
+    //         <a href={this.state.apiData.data.data[3].snippet.thumbnails.default.url} className="thumbnail">
+    //           <img src={this.state.apiData.data.data[3].snippet.thumbnails.default.url} alt="...">
+    //           </img>
+    //         </a>
+    //       </div>
+    //     </div>
+    //   </div>
+    //
+    //     )
+    // }
+    // else {
+    //   return (
+    //     <h1> Loading... </h1>
+    //   )
+    // }
+
+    }
+
   }
 
-}
-
-export default Search
+  export default Search
